4028d1caa30ec33fba5ed7a00f125968d1c9b6c2
Keter <1697339219@qq.com>
2022-07-11 22:02:13 +0800

    Revert "Worker Queue 工作队列补全"
    This reverts commit b7cd6682f457afb0a068189c8bb0f9429e54a29b.

--- src/main/java/indi/keter/rabbitmq/utils/RabbitMqUtils.java
+++ src/main/java/indi/keter/rabbitmq/utils/RabbitMqUtils.java
@@ -10,14 +10,10 @@ public class RabbitMqUtils {
     public static Channel getChannel() throws Exception {
         //创建一个连接工厂
         ConnectionFactory factory = new ConnectionFactory();
-        //工程IP 连接RabbitMQ的队列
         factory.setHost("192.168.81.129");
-        //用户名&密码
         factory.setUsername("keter");
         factory.setPassword("0120");
-        //创建连接
         Connection connection = factory.newConnection();
-        //获取信道
         Channel channel = connection.createChannel();
         return channel;
     }
